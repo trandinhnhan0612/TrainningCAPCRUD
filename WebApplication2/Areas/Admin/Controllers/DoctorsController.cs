b75969a9b260c49c5fb56e6dee10ad138890d871
DinhNhan <trandinhnhan9958@gmail.com>
2021-10-14 21:28:50 +0700

    Trainning CRUD

---
+++ WebApplication2/Areas/Admin/Controllers/DoctorsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Areas.Admin.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private TestEntities db = new TestEntities();
+
+        // GET: Admin/Doctors
+        public ActionResult Index()
+        {
+            var doctors = db.Doctors.Include(d => d.Patient);
+            return View(doctors.ToList());
+        }
+
+        // GET: Admin/Doctors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // GET: Admin/Doctors/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Patients, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Doctors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Department,Description")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Doctors.Add(doctor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Patients, "Id", "Name", doctor.Id);
+            return View(doctor);
+        }
+
+        // GET: Admin/Doctors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Patients, "Id", "Name", doctor.Id);
+            return View(doctor);
+        }
+
+        // POST: Admin/Doctors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Department,Description")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Patients, "Id", "Name", doctor.Id);
+            return View(doctor);
+        }
+
+        // GET: Admin/Doctors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Admin/Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            db.Doctors.Remove(doctor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
